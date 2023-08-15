5d829f4adf371148e2b8157672f8ec1d8f16da3e
Marco Tröster <marco@troester-gmbh.de>
2023-08-15 23:14:10 +0200

    add test case for strassen algo (crashes)

--- FastMatrix.Net.Test/MatmulTest.cs
+++ FastMatrix.Net.Test/MatmulTest.cs
@@ -3,10 +3,25 @@ namespace FastMatrix.Net.Test;
 public class MatmulTest
 {
     [Fact]
-    public void SmallMatmulTest()
+    public void NaiveMatmulTest()
     {
         var id = Matrix.IdentityMatrix(5, 5);
         var rand = Matrix.RandomMatrix(5, 5, 10);
+        var result = Matrix.Multiply(rand, id);
+
+        Assert.Equal(rand.rows, result.rows);
+        Assert.Equal(rand.cols, result.cols);
+
+        for (int row = 0; row < rand.rows; row++)
+            for (int col = 0; col < rand.cols; col++)
+                Assert.Equal(rand[row, col], result[row, col]);
+    }
+
+    [Fact]
+    public void StrassenMatmulTest()
+    {
+        var id = Matrix.IdentityMatrix(64, 64);
+        var rand = Matrix.RandomMatrix(64, 64, 10);
         var result = Matrix.Multiply(rand, id);
 
         Assert.Equal(rand.rows, result.rows);
